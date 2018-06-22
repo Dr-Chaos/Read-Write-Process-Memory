c243922468d01242553d44105e081aa790ea3fd8
Dr-Chaos <crossazriel@gmail.com>
2018-06-22 16:01:18 +0200

    Add files via upload

--- memorymanager.h
+++ memorymanager.h
@@ -10,14 +10,14 @@ class memory_manager
 {
 public:
 	void window(const char* window_name);
-	std::string read(long long address) const;
+	int read_int(long long address) const;
+	std::string read_string(long long address) const;
 	void write_int(long long address, int new_data) const;
 	void write_string(long long address, const std::string& new_data) const;
 
 private:
 	HANDLE application_process = nullptr;
 };
-
 
 
 inline void memory_manager::window(const char* window_name)
@@ -57,34 +57,47 @@ inline void memory_manager::window(const char* window_name)
 	}
 }
 
-
-inline std::string memory_manager::read(const long long address) const
+inline int memory_manager::read_int(const long long address) const
 {
-	DWORD address_value;
+	int address_value;
 	if (ReadProcessMemory(application_process, LPCVOID(address), &address_value, sizeof address_value, nullptr))
 	{
-		//cout << "Succes read process memory, the value is: " << address_value << std::endl;
+		// nothind to do
 	}
 	else
 	{
-		//cout << "Error to read process memory" << std::endl;
-
-		// convert address_value to string
-		std::to_string(address_value) = "Error to read process memory";
+		// convert address_value to string and write error message
+		std::to_string(address_value) = "Error read_int";
 	}
 
-	return std::to_string(address_value);
+	return address_value;
+}
+
+inline std::string memory_manager::read_string(const long long address) const
+{
+	char address_value[256];
+	std::string address_value_string;
+	if(ReadProcessMemory(application_process, LPVOID(address), &address_value, sizeof address_value, nullptr))
+	{
+		address_value_string = std::string(address_value);
+	}
+	else
+	{
+		address_value_string = "Error read_string";
+	}
+
+	return address_value_string;
 }
 
 inline void memory_manager::write_int(const long long address, int new_data) const
 {
 	if (WriteProcessMemory(application_process, LPVOID(address), &new_data, sizeof new_data, nullptr))
 	{
-		std::cout << "Success to write process memory, new value is: " << new_data << std::endl;
+		std::cout << "Success write_int, new value is: " << new_data << std::endl;
 	}
 	else
 	{
-		std::cout << "Error to write process memory" << std::endl;
+		std::cout << "Error write_int" << std::endl;
 	}
 }
 
@@ -92,10 +105,10 @@ inline void memory_manager::write_string(const long long address, const std::str
 {
 	if (WriteProcessMemory(application_process, LPVOID(address), new_data.c_str(), new_data.length() + 1, nullptr))
 	{
-		std::cout << "Success to write process memory, new value is: " << new_data << std::endl;
+		std::cout << "Success write_string, new value is: " << new_data << std::endl;
 	}
 	else
 	{
-		std::cout << "Error to write process memory" << std::endl;
+		std::cout << "Error write_string" << std::endl;
 	}
 }
