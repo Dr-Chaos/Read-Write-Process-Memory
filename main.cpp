70f4e631c509f6c98d7f3c1d38bde61c351a8fd9
Dr-Chaos <crossazriel@gmail.com>
2018-06-23 22:25:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,14 +15,28 @@ int main()
 	memory_manager memory;
 
 	memory.window("Breakpoint World Tennis Tournament");
-	
-	auto address_int = 0x7FF763ADBE4C;
-	cout << "Address value: " << memory.read(address_int) << endl;
-	memory.write_int(address_int, 50);
 
-	auto address_string = 0x9LL193JQWE1P;
-	cout << "Actual address_string value: " << memory.read_string(address_string) << endl;
-	memory.write_string(address_string, "Hello");
+	const auto address_int = 0x7Ff65477BE4C;
+	cout << "address_int value: " << memory.read_int(address_int) << endl;
+	memory.write_int(address_int, 50);
+	// cout << "Type address_int is: " << typeid(memory.read_int(address_int)).name() << endl;
+
+	const auto address_string = 0x7FF654044938;
+	cout << "address_string value: " << memory.read_string(address_string) << endl;
+	memory.write_string(address_string, "Hello World");
+	// cout << "Type address_string is: " << typeid(memory.read_string(address_string)).name() << endl;
+
+
+	const auto module_name = "wttpc.exe";
+
+	const auto offset_address_int = 0x8B6BE4C;
+	cout << "read_base_int value: " << memory.read_base_int(module_name, offset_address_int) << endl;
+	memory.write_base_int(module_name, offset_address_int, 133);
+
+	const auto offset_address_string = 0x8434978;
+	cout << "read_base_string value: " << memory.read_base_string(module_name, offset_address_string) << endl;
+	memory.write_base_string(module_name, offset_address_string, "Yattaaa");
+
 
 	cout << "Press Enter to continue..." << endl;
 	cin.ignore().get();
--- memorymanager.h
+++ memorymanager.h
@@ -8,22 +8,46 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <tchar.h>
+#include <TlHelp32.h>
 
 
 class memory_manager
 {
 public:
-	void window(const char* window_name);
-	int read_int(long long address) const;
-	std::string read_string(long long address) const;
-	void write_int(long long address, int new_data) const;
-	void write_string(long long address, const std::string& new_data) const;
 
-private:
+	void window(const char* window_name);
+
+	// read static_address
+	int read_int(long long static_address) const;
+	std::string read_string(long long static_address) const;
+
+	// write static_address
+	void write_int(long long static_address, int new_data) const;
+	void write_string(long long static_address, const std::string& new_data) const;
+
+	// get module_base_address
+	uintptr_t get_module_base_address(const TCHAR* sz_module_name) const;
+
+	// read module_base_address + offset_address = static_address
+	int read_base_int(const TCHAR* module_name, long long offset_address) const;
+	std::string read_base_string(const TCHAR* module_name, long long offset_address) const;
+
+	// write in module_base_address + offset_address = static_address
+	void write_base_int(const TCHAR* module_name, long long offset_address, int new_data) const;
+	void write_base_string(const TCHAR* module_name, long long offset_address, const std::string& new_data) const;
+
+
+	// global objects
 	HANDLE application_process = nullptr;
+	DWORD process_pid = 0;
 };
 
 
+/**
+ * \brief 
+ * \param window_name 
+ */
 inline void memory_manager::window(const char* window_name)
 {
 	// find the application by window name
@@ -32,15 +56,15 @@ inline void memory_manager::window(const char* window_name)
 	{
 		std::cout << "Success to find the application by window name" << std::endl;
 
-		//Gets the process pid
-		DWORD pid;
-		const auto application_pid = GetWindowThreadProcessId(application_window_name, &pid);
+		// gets the process pid
+		// DWORD process_pid;
+		const auto application_pid = GetWindowThreadProcessId(application_window_name, &process_pid);
 		if (application_pid)
 		{
 			std::cout << "Success to find application pid" << std::endl;
 
-			//Open the process
-			application_process = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+			// open the process
+			application_process = OpenProcess(PROCESS_ALL_ACCESS, false, process_pid);
 			if (application_process)
 			{
 				std::cout << "Success to open the process" << std::endl;
@@ -61,10 +85,15 @@ inline void memory_manager::window(const char* window_name)
 	}
 }
 
-inline int memory_manager::read_int(const long long address) const
+/**
+ * \brief 
+ * \param static_address 
+ * \return 
+ */
+inline int memory_manager::read_int(const long long static_address) const
 {
 	int address_value;
-	if (ReadProcessMemory(application_process, LPCVOID(address), &address_value, sizeof address_value, nullptr))
+	if (ReadProcessMemory(application_process, LPCVOID(static_address), &address_value, sizeof address_value, nullptr))
 	{
 		// nothind to do
 	}
@@ -77,11 +106,16 @@ inline int memory_manager::read_int(const long long address) const
 	return address_value;
 }
 
-inline std::string memory_manager::read_string(const long long address) const
+/**
+ * \brief 
+ * \param static_address 
+ * \return 
+ */
+inline std::string memory_manager::read_string(const long long static_address) const
 {
 	char address_value[256];
 	std::string address_value_string;
-	if(ReadProcessMemory(application_process, LPVOID(address), &address_value, sizeof address_value, nullptr))
+	if (ReadProcessMemory(application_process, LPVOID(static_address), &address_value, sizeof address_value, nullptr))
 	{
 		address_value_string = std::string(address_value);
 	}
@@ -93,9 +127,14 @@ inline std::string memory_manager::read_string(const long long address) const
 	return address_value_string;
 }
 
-inline void memory_manager::write_int(const long long address, int new_data) const
+/**
+ * \brief 
+ * \param static_address 
+ * \param new_data 
+ */
+inline void memory_manager::write_int(const long long static_address, int new_data) const
 {
-	if (WriteProcessMemory(application_process, LPVOID(address), &new_data, sizeof new_data, nullptr))
+	if (WriteProcessMemory(application_process, LPVOID(static_address), &new_data, sizeof new_data, nullptr))
 	{
 		std::cout << "Success write_int, new value is: " << new_data << std::endl;
 	}
@@ -105,9 +144,135 @@ inline void memory_manager::write_int(const long long address, int new_data) con
 	}
 }
 
-inline void memory_manager::write_string(const long long address, const std::string& new_data) const
+/**
+ * \brief 
+ * \param static_address 
+ * \param new_data 
+ */
+inline void memory_manager::write_string(const long long static_address, const std::string& new_data) const
 {
-	if (WriteProcessMemory(application_process, LPVOID(address), new_data.c_str(), new_data.length() + 1, nullptr))
+	if (WriteProcessMemory(application_process, LPVOID(static_address), new_data.c_str(), new_data.length() + 1,
+	                       nullptr))
+	{
+		std::cout << "Success write_string, new value is: " << new_data << std::endl;
+	}
+	else
+	{
+		std::cout << "Error write_string" << std::endl;
+	}
+}
+
+/*
+ * use processus name for return module base address
+ * in Cheat Engine: double click on static address, you need to see: wttpc.exe+84391C4
+ * wttpc.exe is the processus name and 84391C4 is the offset.
+ * So the module_base_address + offset_address = static_address
+ */
+inline uintptr_t memory_manager::get_module_base_address(const TCHAR* sz_module_name) const
+{
+	uintptr_t module_base_address = 0;
+	const auto h_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, process_pid);
+	if (h_snapshot != INVALID_HANDLE_VALUE)
+	{
+		MODULEENTRY32 module_entry32;
+		module_entry32.dwSize = sizeof(MODULEENTRY32);
+		if (Module32First(h_snapshot, &module_entry32))
+		{
+			do
+			{
+				if (_tcsicmp(module_entry32.szModule, sz_module_name) == 0)
+				{
+					module_base_address = uintptr_t(module_entry32.modBaseAddr);
+					break;
+				}
+			}
+			while (Module32Next(h_snapshot, &module_entry32));
+		}
+		CloseHandle(h_snapshot);
+	}
+
+	return module_base_address;
+}
+
+/**
+ * \brief 
+ * \param module_name 
+ * \param offset_address 
+ * \return 
+ */
+inline int memory_manager::read_base_int(const TCHAR* module_name, const long long offset_address) const
+{
+	int address_value;
+	const auto module_base_address = get_module_base_address(module_name);
+	if (ReadProcessMemory(application_process, LPCVOID(module_base_address + offset_address), &address_value,
+	                      sizeof address_value, nullptr))
+	{
+		// nothind to do
+	}
+	else
+	{
+		// convert address_value to string and write error message
+		std::to_string(address_value) = "Error read_int";
+	}
+
+	return address_value;
+}
+
+/**
+ * \brief 
+ * \param module_name 
+ * \param offset_address 
+ * \return 
+ */
+inline std::string memory_manager::read_base_string(const TCHAR* module_name, const long long offset_address) const
+{
+	char address_value[256];
+	std::string address_value_string;
+	const auto module_base_address = get_module_base_address(module_name);
+	if (ReadProcessMemory(application_process, LPVOID(module_base_address + offset_address), &address_value,
+	                      sizeof address_value, nullptr))
+	{
+		address_value_string = std::string(address_value);
+	}
+	else
+	{
+		address_value_string = "Error read_string";
+	}
+
+	return address_value_string;
+}
+
+/**
+ * \brief 
+ * \param module_name 
+ * \param offset_address 
+ * \param new_data 
+ */
+inline void memory_manager::write_base_int(const TCHAR* module_name, const long long offset_address, int new_data) const
+{
+	const auto module_base_address = get_module_base_address(module_name);
+	if (WriteProcessMemory(application_process, LPVOID(module_base_address + offset_address), &new_data,
+	                       sizeof new_data, nullptr))
+	{
+		std::cout << "Success write_base_int, new value is: " << new_data << std::endl;
+	}
+	else
+	{
+		std::cout << "Error write_base_int" << std::endl;
+	}
+}
+
+/**
+ * \brief 
+ * \param module_name 
+ * \param offset_address 
+ * \param new_data 
+ */
+inline void memory_manager::write_base_string(const TCHAR* module_name, const long long offset_address, const std::string& new_data) const
+{
+	const auto module_base_address = get_module_base_address(module_name);
+	if (WriteProcessMemory(application_process, LPVOID(module_base_address + offset_address), new_data.c_str(),
+	                       new_data.length() + 1, nullptr))
 	{
 		std::cout << "Success write_string, new value is: " << new_data << std::endl;
 	}
